c3d2df87c1b94fa8f9f7c5733b19316bb0e75215
pavi <paviguna116@gmail.com>
2024-10-18 23:22:43 +0530

    added index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const Stripe = require('stripe');
+const cors = require("cors");
+const stripe = Stripe('sk_test_51OSGOzSIkNIxCI01wwDbnshaiCV5YiHYu52gnWOiCrvsn3mKD5WXVjWEHdnXwGHbbqRHjfoRiug5P7zDZ1ekC49C00vIuc7WRZ');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+const port = 3000;
+const host = '0.0.0.0';
+
+app.post("/payment-sheet", async(req, res, next) => {
+    try{
+        const data = req.body;
+        console.log(req.body);
+        const params= {
+            email: data.email,
+            name: data.name,
+        };
+        const customer = await stripe.customers.create(params);
+        console.log(customer.id);
+
+        const ephemeralKey = await stripe.ephemeralKeys.create(
+            {customer: customer.id},
+            {apiVersion: '2020-08-27'}
+        );
+
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: parseInt(data.amount),
+            currency: data.currency,
+            customer: customer.id,
+            automatic_payment_methods:{
+                enabled: true,
+            },
+        });
+        const response = {
+            paymentIntent: paymentIntent.client_secret,
+            ephemeralKey: ephemeralKey.secret,
+            customer: customer.id,
+        };
+        res.status(200).send(response);
+    }catch(e){
+        next(e);
+    }
+});
+
+app.listen(port, host, ()=>{
+    console.log(`Server is running at http://${host}:${port}`);
+});
+
